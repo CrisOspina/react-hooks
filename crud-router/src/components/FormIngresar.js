6f787dab66e43769d3c8e0376fc00dfffb821fda
CrisOspina <crisospina523@gmail.com>
2019-10-09 00:51:45 -0500

    [add] form-crud

---
+++ crud-router/src/components/FormIngresar.js
@@ -0,0 +1,137 @@
+import React, { Fragment, useState } from 'react'
+import axios from 'axios'
+
+// Components
+import { RowForm, Error } from './index'
+
+// Hooks
+import { useFormInput } from '../hooks/useFormInput'
+// import { usePOST } from '../hooks/usePOST'
+
+export const FormIngresar = () => {
+  const [error, setError] = useState(false)
+  const nombrePlatillo = useFormInput('')
+  const precioPlatillo = useFormInput('')
+  const categoria = useFormInput('')
+
+  const Nombre = () => {
+    return (
+      <div class="field">
+        <label class="label">
+          Nombre platillo {nombrePlatillo.value === '' ? '👁' : '✔'}
+        </label>
+        <div class="control">
+          <input
+            class="input"
+            type="text"
+            placeholder="Veggie"
+            {...nombrePlatillo}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  const Precio = () => {
+    return (
+      <div class="field">
+        <label class="label">
+          Precio {precioPlatillo.value === '' ? '👁' : '✔'}
+        </label>
+        <div class="control">
+          <input
+            class="input"
+            type="number"
+            placeholder="$20000"
+            {...precioPlatillo}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  const Categoria = () => {
+    return (
+      <Fragment>
+        <label class="label has-text-centered">
+          Categoria {categoria.value === '' ? '👁' : '✔'}
+        </label>
+        <div class="control has-text-centered">
+          <label class="radio">
+            <input type="radio" name="foobar" {...categoria} value="postres" />
+            Postres
+          </label>
+          <label class="radio">
+            <input type="radio" name="foobar" {...categoria} value="bebida" />
+            Bebida
+          </label>
+          <label class="radio">
+            <input type="radio" name="foobar" {...categoria} value="cortes" />
+            Cortes veggie
+          </label>
+          <label class="radio">
+            <input type="radio" name="foobar" {...categoria} value="ensalada" />
+            Ensalada
+          </label>
+        </div>
+      </Fragment>
+    )
+  }
+
+  const botonRegistrar = () => {
+    return (
+      <div className="field">
+        <div className="control">
+          <input
+            type="submit"
+            className="button is-light is-normal is-fullwidth is-inverted is-outlined"
+            value="Ingresar"
+          />
+        </div>
+      </div>
+    )
+  }
+
+  const insertarRegistro = async e => {
+    e.preventDefault()
+    if (
+      nombrePlatillo.value === '' ||
+      precioPlatillo.value === '' ||
+      categoria.value === ''
+    ) {
+      return setError(true)
+    }
+    setError(false)
+
+    try {
+      const url = `http://localhost:4000/restaurant`
+      const resultado = await axios.post(url, {
+        nombrePlatillo: nombrePlatillo.value,
+        precioPlatillo: precioPlatillo.value,
+        categoria: categoria.value,
+      })
+      console.log(resultado)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  return (
+    <Fragment>
+      {error ? <Error mensaje={'Todos los campos son obligatorios'} /> : null}
+      <form onSubmit={insertarRegistro}>
+        {/* nombre */}
+        <RowForm>{Nombre()}</RowForm>
+
+        {/* precio */}
+        <RowForm>{Precio()}</RowForm>
+
+        {/* categoria */}
+        <RowForm>{Categoria()}</RowForm>
+
+        {/* boton registro */}
+        <RowForm>{botonRegistrar()}</RowForm>
+      </form>
+    </Fragment>
+  )
+}
